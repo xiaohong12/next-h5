8873c2adbb2f685cec196174cc5a5adc3b270d1d
henghong.jiang <9058378+henghongjiang@user.noreply.gitee.com>
2024-09-09 14:30:46 +0800

    集成react-vant

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import NextTranspileModules from "next-transpile-modules";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const withTM = NextTranspileModules(["react-vant"]);
 
-const nextConfig = {
+const nextConfig = withTM({
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-};
+});
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import localFont from "next/font/local";
 import variables from "./variables.module.scss";
+import { Button, Cell, Space, Toast } from "react-vant";
+import { Notify } from "react-vant";
+import { useEffect } from "react";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -13,11 +16,39 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+  useEffect(() => {
+    setTimeout(() => {
+      Notify.show("通知内容");
+    }, 1000);
+  });
   return (
     <>
       <div className={variables.test} style={{ color: variables.primaryColor }}>
         小洪
       </div>
+      <Space align="center">
+        <Button type="primary">Button</Button>
+        <Button>Confirm</Button>
+      </Space>
+      <>
+        <Cell title="文字提示" isLink onClick={() => Toast.info("提示内容")} />
+        <Cell
+          title="加载提示"
+          isLink
+          onClick={() => {
+            Toast.loading({
+              message: "加载中...",
+              forbidClick: true,
+            });
+          }}
+        />
+        <Cell
+          title="成功提示"
+          isLink
+          onClick={() => Toast.success("成功文案")}
+        />
+        <Cell title="失败提示" isLink onClick={() => Toast.fail("失败文案")} />
+      </>
     </>
   );
 }
