0581289b23564d53007e0073b47dcea7285fab79
henghong.jiang <9058378+henghongjiang@user.noreply.gitee.com>
2024-09-09 13:56:20 +0800

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
 };
 
 export default nextConfig;
